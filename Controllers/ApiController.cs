a102e9fb93050927284978181d231c2bb1cdc2eb
lin274606 <lin274606@gmail.com>
2023-07-18 22:00:01 +0800

    加入專案檔案。

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjAJAXHW.Models;
+
+namespace prjAJAXHW.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly DemoContext _context;
+
+        public ApiController(DemoContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //載入縣市
+        public IActionResult Cities()
+        {
+            var cities = _context.Address.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+        //根據縣市載入鄉鎮區
+        public IActionResult Districts(string city)
+        {
+            var district = _context.Address.Where(a => a.City == city)
+                .Select(a => a.SiteId).Distinct();
+            return Json(district);
+        }
+        //根據鄉鎮區載入路名
+        public IActionResult Roads(string SiteId)
+        {
+            var roads = _context.Address.Where(a => a.SiteId == SiteId)
+                .Select(a => a.Road);
+            return Json(roads);
+        }
+
+        public IActionResult CheckAccount(Members member)
+        {
+            var datas = _context.Members.Select(x => x.Name).ToList();
+            if (datas.Where(x => x == member.Name).Any())
+            {
+                return Content("1");
+            }
+            return Content("");//空字串代表不存在
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjAJAXHW.Models;
+using System.Diagnostics;
+
+namespace prjAJAXHW.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult employee()
+        {
+            return View();
+        }
+
+        public IActionResult travelspots()
+        {
+            return View();
+        }
+
+        public IActionResult Address()
+        {
+            return View();
+        }
+
+        public IActionResult AddressFetch()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
